dd36e5a5d05a6a59b655866da5eef357bf777cec
Prasundas99 <prasundas115@gmail.com>
2022-03-27 23:23:05 +0530

    feat: aws file upload

--- routes/awsFileUpload.js
+++ routes/awsFileUpload.js
@@ -4,6 +4,6 @@ import { fileUpload } from "../controller/fileUpload.controller.js";
 
 const router = express.Router();
 
-router.route('/file-upload',).post(fileUpload)
+router.route('/aws/file-upload',).post(fileUpload)
 
 export default router
